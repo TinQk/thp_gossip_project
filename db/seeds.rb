5c66e3da273112cb1bdd746c1c0d0fcaf2c3620a
AndreaPlm <palma.a.andrea@gmail.com>
2018-10-30 11:07:31 +0100

    change seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
+require 'faker'
 
 10.times do |i|
-  Gossip.create(title: "title_#{i}", content: "content_#{i}")
+  Gossip.create(author: Faker::BojackHorseman.character, content: Faker::BojackHorseman.quote)
 end
